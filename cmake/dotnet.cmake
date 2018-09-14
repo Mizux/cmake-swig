f387586fe438e05af6a4a338a19726d4b34ae628
Corentin Le Molgat <corentinl@google.com>
2018-09-14 14:38:07 +0200

    Add dotnet support
    - Add dotnet.cmake
    - Add Foo/dotnet
    - Add Bar/dotnet
    - Add FooBar/dotnet

---
+++ cmake/dotnet.cmake
@@ -0,0 +1,8 @@
+# Find dotnet
+find_program(DOTNET_EXECUTABLE dotnet)
+if(NOT DOTNET_EXECUTABLE)
+  message(FATAL_ERROR "Check for dotnet Program: not found")
+else()
+  message(STATUS "Found dotnet Program: ${DOTNET_EXECUTABLE}")
+endif()
+
