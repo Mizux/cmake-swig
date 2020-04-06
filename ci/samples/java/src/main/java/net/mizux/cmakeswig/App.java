b1b3418989245e9696f4e6da3c0be67c391d45a0
Mizux Seiha <mizux.dev@gmail.com>
2020-04-06 13:56:44 +0200

    ci: add java sample
    note: still not use generated package

---
+++ ci/samples/java/src/main/java/net/mizux/cmakeswig/App.java
@@ -0,0 +1,13 @@
+package net.mizux.cmakeswig;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
