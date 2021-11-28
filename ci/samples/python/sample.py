2ac154bd0969d8e47993ee92b37c5d13976dae41
Mizux Seiha <mizux.dev@gmail.com>
2021-11-28 23:23:45 +0100

    ci: fix python sample

--- ci/samples/python/sample.py
+++ ci/samples/python/sample.py
@@ -1,45 +1,65 @@
-#!/usr/bin/env python3
-'''Test API'''
+import cmakeswig
+from cmakeswig.foo import pyFoo
+from cmakeswig.bar import pyBar
+from cmakeswig.foobar import pyFooBar
 
-import cmakeswig.Foo.pyFoo as foo
-import cmakeswig.Bar.pyBar as bar
-import cmakeswig.FooBar.pyFooBar as foobar
+print(f'version: {cmakeswig.__version__}')
 
+# foo
+print(f'Foo: {dir(pyFoo.Foo)}')
 
-'''Test Foo'''
-print(f'pyFoo: ${dir(foo)}')
-foo.fooHello(1)
-foo.fooHello(2147483647) # max int
-foo.fooHello(2147483647+1) # max int + 1
+p = pyFoo.IntPair(3, 5)
+print(f"class IntPair: {dir(p)}")
+print(f"p: {p}")
+pyFoo.free_function(2147483647) # max int
+pyFoo.free_function(2147483647+1) # max int + 1
 
-f = foo.Foo()
-print(f'class Foo: ${dir(f)}')
-f.hello(1)
-f.hello(2147483647)
-f.hello(2147483647+1)
+f = pyFoo.Foo()
+print(f'class Foo: {dir(f)}')
+f.static_function(1)
+f.static_function(2147483647)
+f.static_function(2147483647+1)
+f.set_int(13)
+assert(f.get_int() == 13)
+f.set_int64(31)
+assert(f.get_int64() == 31)
 
+# bar
+print(f'Bar: {dir(pyBar.Bar)}')
 
-'''Test Bar'''
-print(f'pyBar: ${dir(bar)}')
-bar.barHello(1)
-bar.barHello(2147483647) # max int
-bar.barHello(2147483647+1) # max int + 1
+p = pyBar.IntPair(3, 5)
+print(f"class IntPair: {dir(p)}")
+print(f"p: {p}")
+pyBar.free_function(2147483647) # max int
+pyBar.free_function(2147483647+1) # max int + 1
 
-b = bar.Bar()
-print(f'class Bar: ${dir(b)}')
-b.hello(1)
-b.hello(2147483647)
-b.hello(2147483647+1)
+b = pyBar.Bar()
+print(f'class Bar: {dir(b)}')
+b.static_function(1)
+b.static_function(2147483647)
+b.static_function(2147483647+1)
+b.set_int(13)
+assert(b.get_int() == 13)
+b.set_int64(31)
+assert(b.get_int64() == 31)
 
+# foobar
+print(f'FooBar: {dir(pyFooBar.FooBar)}')
 
-'''Test FooBar'''
-print(f'pyFooBar: ${dir(foobar)}')
-foobar.foobarHello(1)
-foobar.foobarHello(2147483647) # max int
-foobar.foobarHello(2147483647+1) # max int + 1
+p = pyFooBar.IntPair(3, 5)
+print(f"class IntPair: {dir(p)}")
+print(f"p: {p}")
+pyFooBar.free_function(2147483647) # max int
+pyFooBar.free_function(2147483647+1) # max int + 1
 
-fb = foobar.FooBar()
-print(f'class FooBar: ${dir(fb)}')
-fb.hello(1)
-fb.hello(2147483647)
-fb.hello(2147483647+1)
+fb = pyFooBar.FooBar()
+print(f'class FooBar: {dir(fb)}')
+fb.static_function(1)
+fb.static_function(2147483647)
+fb.static_function(2147483647+1)
+fb.set_foo_int(13)
+fb.set_bar_int(17)
+assert(fb.get_int() == 30)
+fb.set_foo_int64(31)
+fb.set_bar_int64(37)
+assert(fb.get_int64() == 68)
